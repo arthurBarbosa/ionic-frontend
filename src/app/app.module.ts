1c8fabb7b06a5e5efdc489ec87de311796235e39
Arthur Barbosa <arthurbruno03@gmail.com>
2020-04-23 15:25:57 -0400

    salvando token no localstorage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { HomeModule } from '../pages/home/home.module';
 import { CategoriaService } from '../services/domain/categoria.service';
 import { ErrorInterceptorProvider } from './interceptors/error-interceptor';
 import { AuthService } from '../services/auth.service';
+import { StorageService } from '../services/storage.service';
 
 @NgModule({
   declarations: [
@@ -36,6 +37,7 @@ import { AuthService } from '../services/auth.service';
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     CategoriaService,
     ErrorInterceptorProvider,
+    StorageService,
     AuthService
   ]
 })
