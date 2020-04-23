66ca0d9718a89befc144c9ea45189304c0288dba
Arthur Barbosa <arthurbruno03@gmail.com>
2020-04-23 15:00:18 -0400

    commecando autenticacao

---
+++ src/pages/categoria/categoria.ts
@@ -0,0 +1,36 @@
+import { Component } from "@angular/core";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { CategoriaService } from "../../services/domain/categoria.service";
+import { CategoriaDTO } from "../../models/categoria.dto";
+
+/**
+ * Generated class for the CategoriaPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: "page-categoria",
+  templateUrl: "categoria.html",
+})
+export class CategoriaPage {
+  items: CategoriaDTO[];
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public categoriaService: CategoriaService
+  ) {}
+
+  ionViewDidLoad() {
+    this.categoriaService.findAll().subscribe(
+      (response) => {
+        this.items = response;
+      },
+      (error) => {}
+    );
+
+  }
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,40 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController, IonicPage, MenuController } from "ionic-angular";
+import { CredenciasDTO } from "../../models/credencias.dto";
+import { AuthService } from "../../services/auth.service";
 
+@IonicPage()
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html",
 })
 export class HomePage {
+  creds: CredenciasDTO = {
+    email: "",
+    senha: "",
+  };
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    public menu: MenuController,
+    public auth: AuthService
+  ) {}
 
+  login() {
+    this.auth.authenticate(this.creds).subscribe(
+      (response) => {
+        console.log(response.headers.get("Authorization"));
+        this.navCtrl.push("CategoriaPage");
+      },
+      (error) => {}
+    );
   }
 
+  ionViewWillEnter() {
+    this.menu.swipeEnable(false);
+  }
+
+  ionViewDidLeave() {
+    this.menu.swipeEnable(true);
+  }
 }
