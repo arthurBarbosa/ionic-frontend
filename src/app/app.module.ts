5133e43191731e156d2849a7c5c49e0e7c3ebded
Arthur Barbosa <arthurbruno03@gmail.com>
2020-04-23 20:05:29 -0400

    mostrando dados do cliente na pagina profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { CategoriaService } from '../services/domain/categoria.service';
 import { ErrorInterceptorProvider } from './interceptors/error-interceptor';
 import { AuthService } from '../services/auth.service';
 import { StorageService } from '../services/storage.service';
+import { ClienteService } from '../services/domain/cliente.service';
 
 
 @NgModule({
@@ -38,6 +39,7 @@ import { StorageService } from '../services/storage.service';
     ErrorInterceptorProvider,
     StorageService,
     AuthService,
+    ClienteService
 
   ]
 })
