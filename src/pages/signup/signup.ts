a1e579d45a418824181651b1c40742ea4568116c
Arthur Barbosa <arthurbruno03@gmail.com>
2020-04-25 19:55:47 -0400

    criando formGroup para controlar o formulario

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,29 +1,44 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the SignupPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { Component } from "@angular/core";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @IonicPage()
 @Component({
-  selector: 'page-signup',
-  templateUrl: 'signup.html',
+  selector: "page-signup",
+  templateUrl: "signup.html",
 })
 export class SignupPage {
+  formGroup: FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public formBuilder: FormBuilder
+  ) {
+    this.formGroup = formBuilder.group({
+      nome: ['Joao', [Validators.required,Validators.min(5), Validators.maxLength(120)]],
+      email: ['joaquin@gmail.com', [Validators.required, Validators.email]],
+      tipo : ['1', [Validators.required]],
+      cpfOuCnpj : ['06134596280', [Validators.required, Validators.minLength(11), Validators.maxLength(14)]],
+      senha : ['123', [Validators.required]],
+      logradouro : ['Rua Sobe desce', [Validators.required]],
+      numero : ['25', [Validators.required]],
+      complemento : ['Apto 3', []],
+      bairro : ['Copacabana', []],
+      cep : ['10828333', [Validators.required]],
+      telefone1 : ['977261827', [Validators.required]],
+      telefone2 : ['', []],
+      telefone3 : ['', []],
+      estadoId : [null, [Validators.required]],
+      cidadeId : [null, [Validators.required]]
+    });
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad SignupPage');
+    console.log("ionViewDidLoad SignupPage");
   }
 
-  signupUser(){
-    console.log('enviou o form')
+  signupUser() {
+    console.log("enviou o form");
   }
-
 }
