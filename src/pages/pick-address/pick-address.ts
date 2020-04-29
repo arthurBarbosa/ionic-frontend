fb0ab0e01d506f7b6a2ca577a91a67f2759f2517
Arthur Barbosa <arthurbruno03@gmail.com>
2020-04-29 11:09:14 -0400

    armazenamento dos dados dos pedidos

--- src/pages/pick-address/pick-address.ts
+++ src/pages/pick-address/pick-address.ts
@@ -1,3 +1,5 @@
+import { PedidoDTO } from "./../../models/pedido.dto";
+import { CartService } from "./../../services/domain/cart.service";
 import { StorageService } from "./../../services/storage.service";
 import { EnderecoDTO } from "./../../models/endereco.dto";
 import { Component } from "@angular/core";
@@ -11,23 +13,44 @@ import { ClienteService } from "../../services/domain/cliente.service";
 })
 export class PickAddressPage {
   items: EnderecoDTO[];
+  pedido: PedidoDTO;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     public storage: StorageService,
-    public clienteService: ClienteService
+    public clienteService: ClienteService,
+    public cartService: CartService
   ) {}
 
   ionViewDidLoad() {
     let localUser = this.storage.getLocalUser();
     if (localUser && localUser.email) {
-      this.clienteService.findByEmail(localUser.email)
-      .subscribe(response => {
-        this.items = response['enderecos']
-      },
-      error => {}
-      )
+      this.clienteService.findByEmail(localUser.email).subscribe(
+        (response) => {
+          this.items = response["enderecos"];
+
+          let cart = this.cartService.getCart();
+
+          this.pedido = {
+            cliente: { id: response["id"] },
+            enderecoDeEntrega: null,
+            pagamento: null,
+            itens: cart.items.map((x) => {
+              return {
+                quantidade: x.quantidade,
+                produto: { id: x.produto.id },
+              };
+            }),
+          };
+        },
+        (error) => {}
+      );
     }
+  }
+
+  nextPage(item: EnderecoDTO){
+    this.pedido.enderecoDeEntrega= {id: item.id}
+    console.log(this.pedido)
   }
 }
